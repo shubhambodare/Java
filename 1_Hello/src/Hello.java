d7e44a10c6a08429172820e99e22de691cc4a2d8
Shubham <Shubham@shubham>
2019-07-29 14:55:12 +0530

    ince

--- 1_Hello/src/Hello.java
+++ 1_Hello/src/Hello.java
@@ -2,11 +2,14 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		int a=5;
-		short b=3;
-		b=(short)(a);
-		System.out.println("Hi Shubham:-"+b);
-
+		int i=5;
+		int j;
+		j=i++;//i=6
+		System.out.println(j);//5
+		j=++i;//7
+		System.out.println(j);//7
+		System.out.println(i);//7
+		 
 	}
 
 }
